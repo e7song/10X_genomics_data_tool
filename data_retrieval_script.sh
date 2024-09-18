ef3887f5757e7b160ed8f0016063a8d630b5bee1
Eric Song <e7song@ucsd.edu>
2024-09-17 18:02:42 -0700

    working on wget functionality

--- data_retrieval_script.sh
+++ data_retrieval_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # exit at any error
-set -e 
+# set -e 
 
 # get the URL
 echo "Insert URL: "
@@ -12,15 +12,38 @@ echo "Insert Directory Location: "
 read DIRECTORY
 
 # start the downloading
-echo "Starting Script: "`date`
+echo "Starting Script: "$(date)
 echo "Downloading from $URL. Are you in the correct directory? [y/n]"
 read CONTINUE_DOWNLOAD
 
-if [$CONTINUE_DOWNLOAD != 'y']; then
-    
+if [ "$CONTINUE_DOWNLOAD" != 'y' ]; then
+    echo "Early Exit"
+    exit 0
 fi
 
+if [ -d "DIRECTORY" ]; then
+    echo "Downloading to $DIRECTORY"
+    DELETE_DIR_ON_ERR=1 # don't delete the users pre-created directory
+else
+    echo "Creating $DIRECTORY"
+    mkdir -p "$DIRECTORY"
+    DELETE_DIR_ON_ERR=0 # delete the directory
+fi
 
+wget $URL
+
+# additional comments: maybe filter for the size of the directory? need to look more into this
+
+if [ $? -eq 0 ]; then # checking the success of wget
+    echo "Download successfully finished at: "$(date)
+else
+    echo "Something went wrong with the download."
+    echo "Exited at: "$(date)
+    if [ DELETE_DIR_ON_ERR -eq 0 ]; then
+        rm -rf "$DIRECTORY" # dangerous? might delete all the data... maybe query user decision?
+        rmdir "$DIRECTORY"
+    exit 1
+fi
 
 echo "Testing $URL and $DIRECTORY"
 
